fb14cca7ed2e63f310b1af4fc8eae2b5b5c813da
dolvin17 <huertas876@gmail.com>
2023-12-21 23:56:28 +0100

    adding strncmp for parsing args

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:28:15 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/20 15:41:29 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/21 11:21:27 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,20 @@ void	iferror(bool iferror, int value, char *str)
 		perror(str);
 		exit(EXIT_FAILURE);
 	}
+}
+
+int	ft_strncmp(char	*str1, char *str2, int size)
+{
+	int	i;
+
+	if (str1 == NULL || str2 == NULL || size == 0)
+		return (0);
+	i = 0;
+	while (i <= size)
+	{
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	return (0);
 }

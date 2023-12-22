8a1bec496838b436570b638fbe394db2026853a7
dolvin17 <huertas876@gmail.com>
2023-12-22 16:35:00 +0100

    adding atoi fn

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:28:15 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/21 11:21:27 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/22 15:57:16 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,25 @@ int	ft_strncmp(char	*str1, char *str2, int size)
 		i++;
 	}
 	return (0);
+}
+
+int	ft_atoi(char	*str)
+{
+	int	sign;
+	int	nbr;
+	int	i;
+
+	sign = 1;
+	nbr = 0;
+	i = 0;
+	if (str[i] == '-')
+		sign *= -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nbr * sign);
 }

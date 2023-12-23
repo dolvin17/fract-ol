89d06fcd027e62800e5b7e5cdfaa551af59caa86
dolvin17 <huertas876@gmail.com>
2023-12-23 17:00:37 +0100

    testing mlx, adding structs

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:19:21 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/22 22:35:56 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/23 17:00:09 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,23 @@ typedef struct s_complex
 	double	real;
 	double	i;
 }	t_complex;
+//variables necesarias para crear la imagen
+typedef struct s_data
+{
+	void	*image; //puntero a la imagen
+	char	*adress; //dirección de la imagen
+	int		bbp; //bites por pixel
+	int		line_len; //tamaño
+	int		endian; //tamaño, aparentemente useless en macos
+}	t_data;
+
+typedef struct s_fractol
+{
+	void	*ptr_mlx; //iniciar la mlx
+	void	*open_w;  //abrir una ventana
+	int		keep_open;//mantener la ventana
+	t_data	*image;   //crear una imagen
+}	t_fractol;
 
 //Parsing errors
 void	iferror(bool iferror, int value, char *str);

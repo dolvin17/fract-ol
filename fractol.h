bea35f17a65c96abf04588e82db4cae4c0dc4623
dolvin17 <huertas876@gmail.com>
2023-12-24 01:20:55 +0100

    adding fn rendering_fractol && defined colors

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:19:21 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/23 22:28:59 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/24 01:16:29 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,14 @@ typedef struct s_fractol
 
 # define WIDTH 800
 # define HEIGHT 800
+//colors for fractal
+# define NEON_PINK 0xFF6EC7
+# define NEON_GREEN 0x39FF14
+# define NEON_BLUE 0x00BFFF
+# define NEON_YELLOW 0xFFFF00
+# define NEON_ORANGE 0xFFA500
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
 //Parsing errors
 void		iferror(bool iferror, int value, char *str);
 int			ft_strncmp(char	*str1, char *str2, int size);
@@ -58,4 +66,5 @@ double		scale(double value, double new_min, double new_max, double old_min, doub
 t_complex	sum_complex(t_complex z1, t_complex z2);
 //cuadrado de un numero complejo. i^2 = -1
 t_complex	square_z(t_complex z);
+void	rendering_fractol(t_fractol *fractol);
 #endif

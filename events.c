066266c2de65a3a7bdb1a640b9410f3979e1c88f
dolvin17 <huertas876@gmail.com>
2023-12-27 12:56:39 +0100

    testing x11 lib

--- events.c
+++ events.c
@@ -6,21 +6,30 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 20:21:22 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/25 22:13:32 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/26 19:36:51 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-
-int	key_control(int keysym, t_fractol *fractol)
+int	close_handler(t_fractol *fractol)
 {
-	printf("%d", keysym);
-	printf("%p", fractol);
+	mlx_destroy_image(fractol->ptr_mlx, fractol->image.img_ptr);
+	mlx_destroy_window(fractol->ptr_mlx, fractol->open_w);
+	exit(EXIT_SUCCESS);
+}
+
+int	key_control(int keycode, t_fractol *fractol)
+{
+	if (keycode == XK_Escape)
+		close_handler(fractol);
+	printf("%d", keycode);
+	printf("%p", fractol->ptr_mlx);
+	rendering_fractol(fractol);
 	return (0);
 }
 
 void	hook_events(t_fractol *fractol)
 {
-	mlx_hook(fractol->open_w, KeyPress, KeyPressMask, key_control, fractol);
+	mlx_hook(fractol->open_w, KeyPress, 0L, key_control, fractol);
 }

8a60956cf3780c769eb3f8cb1da6c32f8780f165
ghuertas <ghuertas@student.42madrid.com>
2023-12-27 19:41:39 +0100

    adding mouse control -initial hook && zoom_in && zoom_out fn

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 20:21:22 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/27 17:57:23 by ghuertas         ###   ########.fr       */
+/*   Updated: 2023/12/27 19:25:38 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,48 @@ int	key_control(int keycode, t_fractol *fractol)
 	rendering_fractol(fractol);
 	return (0);
 }
+void	zoom_in(t_fractol	*fractol, double mouse_x, double mouse_y)
+{
+	double zoom_factor;
+	
+	zoom_factor = 0.95;
+
+	fractol->move_x += (mouse_x - fractol->move_x) * (1 - zoom_factor);
+	fractol->move_y += (mouse_y - fractol->move_y) * (1 - zoom_factor);
+	fractol->zoom *= zoom_factor;
+}
+
+void	zoom_out(t_fractol	*fractol, double mouse_x, double mouse_y)
+{
+	double zoom_factor;
+	
+	zoom_factor = 1.05;
+
+	fractol->move_x += (mouse_x - fractol->move_x) * (1 - zoom_factor);
+	fractol->move_y += (mouse_y - fractol->move_y) * (1 - zoom_factor);
+	fractol->zoom *= zoom_factor;
+}
+int	mouse_control(int keycode, int x, int y, t_fractol *fractol)
+{	
+	double i;
+	double real;
+
+	real = (x - WIDTH / 2) / (0.5 * WIDTH * fractol->zoom) + fractol->move_x;
+	i = (y - HEIGHT / 2) / (0.5 * HEIGHT * fractol->zoom) + fractol->move_y;
+	
+	
+	printf("x posicion %d\n", x);
+	printf("y posicion: %d\n", y);
+	if (keycode == MOUSE_UP)
+		zoom_in(fractol, real, i);
+	else if (keycode == MOUSE_DOWN)
+		zoom_out(fractol, real, i);
+	rendering_fractol(fractol);
+	return (0);
+}
 
 void	hook_events(t_fractol *fractol)
 {
-	mlx_hook(fractol->open_w, 02, 0L, key_control, fractol);  
+	mlx_hook(fractol->open_w, 02, 0L, key_control, fractol);
+	mlx_hook(fractol->open_w, 04, 0L, mouse_control, fractol);
 }

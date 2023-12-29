d08b366d99ce0782ea7ab09c75f0581be3f63edb
dolvin17 <huertas876@gmail.com>
2023-12-29 23:43:36 +0100

    using hypot fn, removing hypotenuse var

--- fractol.c
+++ fractol.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 17:47:47 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/27 17:50:07 by ghuertas         ###   ########.fr       */
+/*   Updated: 2023/12/29 23:29:56 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	init_util_data(t_fractol *fractol)
 {
-	fractol->iterations = 42;
-	fractol->hypotenuse = 4;
+	fractol->iterations = 10;
 	fractol->move_x = 0.0;
 	fractol->move_y = 0.0;
 	fractol->zoom = 1.0;
+
 }
 // funcion para inicializar el struct
 void	init_fractol(t_fractol *fractol)

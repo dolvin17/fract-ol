0ed44ddcd313dcabf6b01d76fe2c0f7688847ab7
dolvin17 <huertas876@gmail.com>
2023-12-29 23:48:21 +0100

    removing julia fn, setting inputs as numbers

--- main.c
+++ main.c
@@ -6,17 +6,12 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:24:35 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/28 22:16:36 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/29 22:20:01 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	julia(char	**real, char **imaginary, t_fractol *fractol)
-{
-	fractol->julia_real = ft_atof(*real);
-	fractol->julia_i = ft_atof(*imaginary);
-}
 
 int	main(int argc, char **argv)
 {
@@ -26,8 +21,47 @@ int	main(int argc, char **argv)
 	{
 		if (argv[1] && ft_strncmp(argv[1], "julia", 5) == 0)
 		{
-			fractol.name = argv[1];
-			julia(&argv[2], &argv[3], &fractol);
+			if (atoi(argv[2]) == 1)
+			{
+					fractol.julia_real = 0.285;
+					fractol.julia_i = -0.01;
+			}
+			else if (atoi(argv[2]) == 2)
+			{
+					fractol.julia_real = 0.32;
+					fractol.julia_i = 0.043;
+			}
+			else if (atoi(argv[2]) == 3)
+			{
+					fractol.julia_real = -0.624;
+					fractol.julia_i = 0.435;
+			}
+			else if (atoi(argv[2]) == 4)
+			{
+					fractol.julia_real = -0.8;
+					fractol.julia_i = 0.156;
+			}
+			else if (atoi(argv[2]) == 5)
+			{
+					fractol.julia_real = 0.285;
+					fractol.julia_i = 0;
+			}
+			else if (atoi(argv[2]) == 6)
+			{
+					fractol.julia_real = -0.712;
+					fractol.julia_i = 0.264;
+			}
+			else if (atoi(argv[2]) == 7)
+			{
+					fractol.julia_real = -0.755;
+					fractol.julia_i = 0.15;
+			}
+				else if (atoi(argv[2]) == 8)
+			{
+					fractol.julia_real = -0.70176;
+					fractol.julia_i = -0.3842;
+			}
+			
 			fractol.name = argv[1];
 			init_fractol(&fractol);
 			rendering_fractol(&fractol);

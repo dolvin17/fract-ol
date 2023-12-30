40d73b67e30d7bb1ea5fcfa44eb6dbba5dcd4e59
dolvin17 <huertas876@gmail.com>
2023-12-30 22:29:39 +0100

    restructuring code and adding feat for color changes

--- events.c
+++ events.c
@@ -6,22 +6,21 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 20:21:22 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/30 01:12:38 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/30 22:18:00 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	close_handler(t_fractol *fractol)
+static	int	close_handler(t_fractol *fractol)
 {
 	mlx_destroy_image(fractol->ptr_mlx, fractol->image.img_ptr);
 	mlx_destroy_window(fractol->ptr_mlx, fractol->open_w);
 	exit(EXIT_SUCCESS);
 }
 
-int	key_control(int keycode, t_fractol *fractol)
+static	int	key_control(int keycode, t_fractol *fractol)
 {
-	printf("tecla %d\n", keycode);
 	if (keycode == KEY_ESC)
 		close_handler(fractol);
 	else if (keycode == KEY_UP)
@@ -36,32 +35,26 @@ int	key_control(int keycode, t_fractol *fractol)
 		fractol->iterations += 10;
 	else if (keycode == KEY_MINUS)
 		fractol->iterations -= 10;
+	else if (keycode == KEY_SPACE)
+	{
+		if (fractol->color == NEON_RED)
+			fractol->color = NEON_GREEN;
+		else if (fractol->color == NEON_GREEN)
+			fractol->color = BLACK;
+		else
+			fractol->color = NEON_RED;
+	}
 	rendering_fractol(fractol);
 	return (0);
 }
 
-void	zoom_out(t_fractol	*fractol, double mouse_x, double mouse_y)
-{
-	fractol->zoom /= 1.05 * 0.5;
-	fractol->move_x += mouse_x / (0.5 * WIDTH * fractol->zoom);
-	fractol->move_y += mouse_y / (0.5 * HEIGHT * fractol->zoom);
-}
-void	zoom_in(t_fractol	*fractol, double mouse_x, double mouse_y)
-{
-	fractol->zoom *= 0.95 * 0.5;
-	fractol->move_x -= mouse_x / (0.5 * WIDTH * fractol->zoom);
-	fractol->move_y -= mouse_y / (0.5 * HEIGHT * fractol->zoom);
-}
-
-int	mouse_control(int keycode, int x, int y, t_fractol *fractol)
+static	int	mouse_control(int keycode, int x, int y, t_fractol *fractol)
 {	
-	double i;
-	double real;
+	double	i;
+	double	real;
 
 	real = x / WIDTH;
-	i = y / WIDTH;
-	printf("x posicion %d\n", x);
-	printf("y posicion: %d\n", y);
+	i = y / HEIGHT;
 	if (keycode == MOUSE_UP)
 		zoom_in(fractol, real, i);
 	else if (keycode == MOUSE_DOWN)
@@ -74,4 +67,5 @@ void	hook_events(t_fractol *fractol)
 {
 	mlx_hook(fractol->open_w, 02, 0L, key_control, fractol);
 	mlx_hook(fractol->open_w, 04, 0L, mouse_control, fractol);
+	mlx_hook(fractol->open_w, 17, 0L, close_handler, fractol);
 }

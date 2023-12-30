5eb151df2d9c10c1d861a555f0e0ffe061fd63c7
dolvin17 <huertas876@gmail.com>
2023-12-30 22:39:58 +0100

    unified fractol iterations: refactored, included addition cube z + c for multibrot

--- rendering_fractol.c
+++ rendering_fractol.c
@@ -6,17 +6,13 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 18:50:32 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/12/30 01:34:27 by dolvin17         ###   ########.fr       */
+/*   Updated: 2023/12/30 21:58:21 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-/*mlx_pixel_put function is very, very slow. This is because it tries
-to push the pixel instantly to the window (without waiting for the frame to be entirely rendered). 
-Because of this sole reason, we will have to buffer all of our pixels to a image, 
-which we will then push to the window.*/
-void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
+static void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
 {
 	char	*dst;
 
@@ -24,7 +20,7 @@ void	my_mlx_pixel_put(t_data *img, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void	mandelbrot_or_julia(t_complex *z, t_complex *c, t_fractol *fractol)
+static void	mand_mult_or_jul(t_complex *z, t_complex *c, t_fractol *fractol)
 {
 	if (!(ft_strncmp(fractol->name, "julia", 5)))
 	{
@@ -38,35 +34,34 @@ void	mandelbrot_or_julia(t_complex *z, t_complex *c, t_fractol *fractol)
 	}
 }
 
-//fn to get set complex numbers
-static	void	coordenates(int x, int	y, t_fractol *fractol)
+static	void	coordenates(int x, int y, t_fractol *fractol)
 {
 	t_complex	z;
 	t_complex	c;
 	int			i;
-	int			color;
-	//1 iteracion
-	z.real = (scale(x, -2, 2, 0, WIDTH) * fractol->zoom) + fractol->move_x;
-	z.i = (scale(y, 2, -2, 0, HEIGHT) * fractol->zoom) + fractol->move_y;
+	int			custom;
+
+	z.real = (scale(x, -2, 2, WIDTH) * fractol->zoom) + fractol->move_x;
+	z.i = (scale(y, 2, -2, HEIGHT) * fractol->zoom) + fractol->move_y;
 	i = 0;
-	mandelbrot_or_julia(&z, &c, fractol);
-	//while n cantidad de iteraciones de Z, checking if point converge o diverge
+	mand_mult_or_jul(&z, &c, fractol);
 	while (i < fractol->iterations)
 	{
-		//z = z^2 + c
-		z = sum_complex(square_z(z), c);
-		//si diverge
-		if (hypotenuse(z) > 2)
+		if (!(ft_strncmp(fractol->name, "multibrot", 9)))
+			z = sum_complex(cube_z(z), c);
+		else
+			z = sum_complex(square_z(z), c);
+		if (hypot(z.real, z.i) > 2)
 		{
-			color = scale(i, NEON_YELLOW, NEON_PINK, 0, fractol->iterations);
-			my_mlx_pixel_put(&fractol->image, x, y, color);
+			custom = scale(i, NEON_YELLOW, NEON_PINK, fractol->iterations);
+			my_mlx_pixel_put(&fractol->image, x, y, custom);
 			return ;
 		}
 		++i;
 	}
-	//los puntos convergen
-    my_mlx_pixel_put(&fractol->image, x, y, NEON_RED);
+	my_mlx_pixel_put(&fractol->image, x, y, fractol->color);
 }
+
 void	rendering_fractol(t_fractol *fractol)
 {
 	int	x;
@@ -79,5 +74,6 @@ void	rendering_fractol(t_fractol *fractol)
 		while (++x < WIDTH)
 			coordenates(x, y, fractol);
 	}
-	mlx_put_image_to_window(fractol->ptr_mlx, fractol->open_w, fractol->image.img_ptr, 0, 0);
+	mlx_put_image_to_window(fractol->ptr_mlx,
+		fractol->open_w, fractol->image.img_ptr, 0, 0);
 }
